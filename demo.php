c57dd4d00500f0c0135e26b58b716d97110d44ca
周建军 <773729704@qq.com>
2019-04-03 08:43:38 +0800

    Update demo.php

--- demo.php
+++ demo.php
@@ -9,10 +9,10 @@ require_once './vendor/autoload.php';
 
 try {
     $client = new \GeTui\Client([
-        'app_key' => 'Xw27x1BaCw6xM0LEoyduM7',
-        'app_id' => 'E8oOecFjRt58z1BUAHl2nA',
-        'master_secret' => 'VgNT0H5JXtAKrwqJLIXq19',
-        'logo_url' => 'http://dev.img.ybzg.com/static/app/user/getui_logo.png',
+        'app_key' => '',
+        'app_id' => '',
+        'master_secret' => '',
+        'logo_url' => '',
     ]);
 
 
